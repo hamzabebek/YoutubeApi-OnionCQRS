7e923ab541bcefa44a7db39b8c761d4138073b7b
hamzabebek <hamzabebek1903@gmail.com>
2024-11-01 18:37:26 +0300

    Entity oluşturmak ve çoka çok tablolar

---
+++ Core/YoutubeApi.Domain/Entities/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Brand(string name) : EntityBase
+    {
+        public required string Name { get; set; } = name;
+    }
+}
---
+++ Core/YoutubeApi.Domain/Entities/Detail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Detail : EntityBase
+    {
+        public Detail(string title, string description,int categotyid) {
+        
+            Title = title;
+            Description = description;
+            CategoryId = categotyid;
+        }
+
+        public required string Title { get; set; }
+
+        public required string Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
